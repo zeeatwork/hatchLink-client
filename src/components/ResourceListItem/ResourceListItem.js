112a1308067151d329785a3f9e0d7f27632c744b
Zenzi Ali <zeeathome935@gmail.com>
2020-09-03 16:40:05 -0400

    Updated resource components

---
+++ src/components/ResourceListItem/ResourceListItem.js
@@ -0,0 +1,72 @@
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { NiceDate, Hyph } from '../Utils/Utils'
+import StyleIcon from '../StyleIcon/StyleIcon'
+import './ResourceListItem.css'
+
+export default class ResourceListItem extends Component {
+  render() {
+    const { resource } = this.props
+    return (
+      <Link to={`/resource/${resource.id}`} className='ResourceListItem'>
+        <header className='ResourceListItem__header'>
+          <h2 className='ResourceListItem__heading'>
+            {resource.name}
+          </h2>
+          <ResourceDate resource={resource} />
+        </header>
+        <footer className='ResourceListItem__footer'>
+          <ResourceStyle resource={resource} />
+          {resource.author.id && <>
+            <Hyph />
+            <ResourceReviewAuthor resource={resource} />
+          </>}
+          <ResourceReviewCount resource={resource} />
+        </footer>
+      </Link>
+    )
+  }
+}
+
+function ResourceStyle({ resource }) {
+  return (
+    <span className='ResourceListItem__style'>
+      <StyleIcon style={resource.style} />
+      {' '}
+      {resource.style}
+    </span>
+  )
+}
+
+function ResourceDate({ resource }) {
+  return (
+    <span className='ResourceListItem__date'>
+      <NiceDate
+        date={resource.date_created}
+      />
+    </span>
+  )
+}
+
+function ResourceReviewAuthor({ review }) {
+  return (
+    <span className='Review__author'>
+      {review.author.user_name}
+    </span>
+  )
+}
+
+function ResourceReviewCount({ resource,review }) {
+  return (
+    <span
+      className='ResourceListItem__comment-count fa-layers fa-fw'
+    >
+      <FontAwesomeIcon size='lg' icon='comment' />
+      <span
+        className='fa-layers-text fa-inverse'>
+        {resource.number_of_reviews}
+      </span>
+    </span>
+  )
+}
---
+++ src/components/ReviewForm/ReviewForm.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react'
+import ResourceContext from '../../contexts/ResourceContext'
+import ResourceApiService from '../../services/resource-api-service'
+import { Button, Textarea } from '../Utils/Utils'
+import './ReviewForm.css'
+
+export default class ReviewForm extends Component {
+  static contextType = ResourceContext
+
+  handleSubmit = ev => {
+    ev.preventDefault()
+    const { resource } = this.context
+    const { comment } = ev.target
+    ResourceApiService.postReview(resource.id, comment.value)
+      .then(this.context.addReview)
+      .then(() => {
+        comment.value = ''
+      })
+      .catch(this.context.setError)
+  }
+
+  render() {
+    return (
+      <form
+        className='ReviewForm'
+        onSubmit={this.handleSubmit}
+      >
+        <div className='text'>
+          <Textarea
+            required
+            aria-label='Type a comment...'
+            name='text'
+            id='text'
+            cols='30'
+            rows='3'
+            placeholder='Type a comment..'>
+          </Textarea>
+        </div>
+        <Button type='submit'>
+          Post comment
+        </Button>
+      </form>
+    )
+  }
+}
