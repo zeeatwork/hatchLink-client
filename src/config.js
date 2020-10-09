8e80d1013a43ed4f0010c0dbc570cfafc7a8ca89
Zenzi Ali <zeeathome935@gmail.com>
2020-10-09 18:02:43 -0400

    update confog

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export default {
-  API_ENDPOINT: "https://blooming-reef-68223.herokuapp.com/api/",
+  API_ENDPOINT: "https://blooming-reef-68223.herokuapp.com/api",
   TOKEN_KEY: "hatchlink-client-auth-token",
 };
 
-//https://blooming-reef-68223.herokuapp.com/api/
+//https://blooming-reef-68223.herokuapp.com/api
